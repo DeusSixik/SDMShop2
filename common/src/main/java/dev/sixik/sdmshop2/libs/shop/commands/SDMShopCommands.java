f9d3ad9ae63b77cb3a2225e3bbe973db7589f6b6
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-04-19 13:35:02 +0300

    feat: New Command builder

--- common/src/main/java/dev/sixik/sdmshop2/libs/shop/commands/SDMShopCommands.java
+++ common/src/main/java/dev/sixik/sdmshop2/libs/shop/commands/SDMShopCommands.java
@@ -2,6 +2,7 @@ package dev.sixik.sdmshop2.libs.shop.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.ArgumentBuilder;
+import dev.sixik.sdmshop2.libs.shop.commands.builder.CommandBuilder;
 import dev.sixik.sdmshop2.libs.shop.network.ShopNetworkManager;
 import dev.sixik.sdmshop2.tests.economy.EconomyTest;
 import net.minecraft.commands.CommandBuildContext;
@@ -10,29 +11,26 @@ import net.minecraft.commands.Commands;
 
 public class SDMShopCommands {
 
-    public static void registerCommands(CommandDispatcher<CommandSourceStack> commandSourceStackCommandDispatcher, CommandBuildContext commandBuildContext, Commands.CommandSelection commandSelection) {
-        registerCommands(commandSourceStackCommandDispatcher);
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context, Commands.CommandSelection selection) {
+        registerCommands(dispatcher);
     }
 
     private static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(
-                Commands.literal("sdm_shop")
-                        .then(Commands.literal("open_shop")
-                        )
-                        .then(Commands.literal("test")
-                                .executes(s -> {
-                                    EconomyTest.commandTest(s.getSource().getPlayerOrException());
-                                    return 0;
-                                })
-                        )
-                        .then(Commands.literal("synchronization").requires(s -> s.hasPermission(2)).then(synchronizationCommands()))
-        );
-    }
 
-    private static ArgumentBuilder<CommandSourceStack, ?> synchronizationCommands() {
-        return Commands.literal("limiter_data").executes(commandContext -> {
-            ShopNetworkManager.sendLimiterData(commandContext.getSource().getPlayerOrException());
-            return 0;
-        });
+        CommandBuilder.create("sdm_shop open_shop")
+                .register(dispatcher);
+
+        CommandBuilder.create("sdm_shop test")
+                .executesVoid(ctx -> {
+                    EconomyTest.commandTest(ctx.getSource().getPlayerOrException());
+                })
+                .register(dispatcher);
+
+        CommandBuilder.create("sdm_shop synchronization limiter_data")
+                .requires(2)
+                .executesVoid(ctx -> {
+                    ShopNetworkManager.sendLimiterData(ctx.getSource().getPlayerOrException());
+                })
+                .register(dispatcher);
     }
 }
